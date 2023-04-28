d526d58e7ffb6151d0b3e8f49dcfaf9cedf09693
Gyan kumar <2000031953@kluniversity.in>
2023-04-28 23:56:37 +0530

    First commit created a directory structure

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+
+const app=express();
+
+const port=8000;
+
+app.listen(port,function(err){
+    if(err){
+        console.log("error occured while running on the port ",err);
+    }
+    console.log("The server is running on the port",port);
+});
